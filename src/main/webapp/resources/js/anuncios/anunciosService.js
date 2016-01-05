1fbf5e54e815d9f61613e8239e465ec324644f63
JosueZelaya <josue.a.zelaya@gmail.com>
2016-01-04 23:29:18 -0600

    Ya funciona la carga de imagenes usando ng-flow y XMLHttpRequest, se agregó también un service llamado Publicacion que usaremos como Entidad.

--- src/main/webapp/resources/js/anuncios/anunciosService.js
+++ src/main/webapp/resources/js/anuncios/anunciosService.js
@@ -7,7 +7,50 @@ modulo_anuncios.service('anunciosService', ['$http', function ($http) {
             {
                 return response;
             });
-    }
+    };
     
+    
+    this.agregarPublicacion = function(publicacion){
+        
+        var formData = new FormData();
+        formData.append("titulo",publicacion.titulo);
+        formData.append("precio",publicacion.precio);
+        formData.append("descripcion",publicacion.descripcion);
+        
+        //Se agregan todas las imagenes... Cada una debe llamarse: multipleFiles        
+        //Para que Spring entienda que están relacionadas y que se trata de una colección o lista
+        for (var index = 0; index < publicacion.imagenes.length; ++index) {            
+            formData.append("multipleFiles",publicacion.imagenes[index]);
+        }
+        
+        var request = new XMLHttpRequest();
+        request.open('POST',"/publicacion/agregarPublicacion");
+        request.send(formData);
+    };
 
 }]);
+
+
+modulo_anuncios.service('Publicacion', function(){
+    
+    /**
+    * Constructor, with class name
+    */
+    function Publicacion(titulo, precio, descripcion) {
+        // Public properties, assigned to the instance ('this')
+        this.titulo = titulo;
+        this.precio = precio;
+        this.descripcion = descripcion;
+//        this.imagenes = imagenes;
+    }
+  
+    /**
+    * Public method, assigned to prototype
+    */
+   Publicacion.prototype.setImagenes = function (imagenes) {
+     this.imagenes = imagenes;
+   };
+  
+    return Publicacion;
+  
+});
