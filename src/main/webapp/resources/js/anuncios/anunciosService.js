8f098beb27f95d74e13f6b8acb1a53a2cc0802fb
JosueZelaya <josue.a.zelaya@gmail.com>
2016-01-06 22:34:14 -0600

    Se cierra el modal al agregar una nueva publicacion

--- src/main/webapp/resources/js/anuncios/anunciosService.js
+++ src/main/webapp/resources/js/anuncios/anunciosService.js
@@ -1,40 +1,47 @@
-modulo_anuncios.service('anunciosService',function ($http) {
-    
-    this.getAnuncios = function (tipo,page) {
-        var req = "/publicacion/getAnuncios/"+tipo+"/"+page;
-        return $http.get(req)            
-           .success(function (response)
-            {
-                return response;
-            });
+modulo_anuncios.service('anunciosService', function ($http, $log) {
+
+    this.getAnuncios = function (tipo, page) {
+        var req = "/publicacion/getAnuncios/" + tipo + "/" + page;
+        return $http.get(req)
+                .success(function (response)
+                {
+                    return response;
+                });
     };
-    
-    
-    this.agregarPublicacion = function(publicacion){
-        
+
+
+    this.agregarPublicacion = function (publicacion, callback) {
+
         var formData = new FormData();
-        formData.append("titulo",publicacion.titulo);
-        formData.append("precio",publicacion.precio);
-        formData.append("descripcion",publicacion.descripcion);
-        
+        formData.append("titulo", publicacion.titulo);
+        formData.append("precio", publicacion.precio);
+        formData.append("descripcion", publicacion.descripcion);
+
         //Se agregan todas las imagenes... Cada una debe llamarse: multipleFiles        
         //Para que Spring entienda que están relacionadas y que se trata de una colección o lista
-        for (var index = 0; index < publicacion.imagenes.length; ++index) {            
-            formData.append("multipleFiles",publicacion.imagenes[index]);
+        for (var index = 0; index < publicacion.imagenes.length; ++index) {
+            formData.append("multipleFiles", publicacion.imagenes[index]);
         }
-        
-        var request = new XMLHttpRequest();
-        request.open('POST',"/publicacion/agregarPublicacion");
-        request.send(formData);
+
+        $http.post('/publicacion/agregarPublicacion', formData, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+            callback();
+        })
+        .error(function(){
+        });     
+
     };
 
 });
 
-modulo_anuncios.service('Publicacion', function(){
-    
+modulo_anuncios.service('Publicacion', function () {
+
     /**
-    * Constructor, with class name
-    */
+     * Constructor, with class name
+     */
     function Publicacion(titulo, precio, descripcion) {
         // Public properties, assigned to the instance ('this')
         this.titulo = titulo;
@@ -42,14 +49,14 @@ modulo_anuncios.service('Publicacion', function(){
         this.descripcion = descripcion;
 //        this.imagenes = imagenes;
     }
-  
+
     /**
-    * Public method, assigned to prototype
-    */
-   Publicacion.prototype.setImagenes = function (imagenes) {
-     this.imagenes = imagenes;
-   };
-  
+     * Public method, assigned to prototype
+     */
+    Publicacion.prototype.setImagenes = function (imagenes) {
+        this.imagenes = imagenes;
+    };
+
     return Publicacion;
-  
+
 });
