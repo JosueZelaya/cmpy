e8ad77ec11d52dcdd8d4f6f80d26b542c832a249
JosueZelaya <josue.a.zelaya@gmail.com>
2016-08-19 18:51:16 -0600

    fixed bug missing class

---
+++ src/main/java/com/tecnogeek/comprameya/repositories/impl/PublicacionRepositoryImpl.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecnogeek.comprameya.repositories.impl;
+
+import com.mysema.query.types.Predicate;
+import com.tecnogeek.comprameya.entidad.Publicacion;
+import com.tecnogeek.comprameya.entidad.QPublicacion;
+import com.tecnogeek.comprameya.enums.TipoPublicacionEnum;
+import com.tecnogeek.comprameya.repositories.PublicacionRepository;
+import com.tecnogeek.comprameya.repositories.custom.PublicacionRepositoryCustom;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ *
+ * @author alexander
+ */
+public class PublicacionRepositoryImpl implements PublicacionRepositoryCustom{
+
+    @Autowired
+    PublicacionRepository publicacionRepository;
+    
+    private final QPublicacion qPublicacion = QPublicacion.publicacion;
+    
+    @Override
+    public Publicacion getPublicacion(long publicacion_id) {
+        Predicate porId = qPublicacion.id.eq(publicacion_id);
+        return publicacionRepository.findOne(porId);
+    }
+
+    @Override
+    public Iterable<Publicacion> getPublicacionesPagadas() {
+        Long id = TipoPublicacionEnum.PAGADA.getCodigo();
+        Predicate sonPagadas = qPublicacion.fkTipoPublicacion.tipoPublicacionId.eq(id);
+        return publicacionRepository.findAll(sonPagadas);
+    }
+
+    @Override
+    public Iterable<Publicacion> getPublicacionesGratis() {
+        Long id = TipoPublicacionEnum.GRATIS.getCodigo();
+        Predicate sonGratis = qPublicacion.fkTipoPublicacion.tipoPublicacionId.eq(id);
+        return publicacionRepository.findAll(sonGratis);
+    }
+
+    @Override
+    public Iterable<Publicacion> getPublicacionesPagadas(PageRequest pageRequest) {
+        Long id = TipoPublicacionEnum.PAGADA.getCodigo();
+        Predicate sonPagadas = qPublicacion.fkTipoPublicacion.tipoPublicacionId.eq(id);
+        Page page = publicacionRepository.findAll(sonPagadas, pageRequest);
+        return page.getContent();
+    }
+
+    @Override
+    public Iterable<Publicacion> getPublicacionesGratis(PageRequest pageRequest) {
+        Long id = TipoPublicacionEnum.GRATIS.getCodigo();
+        Predicate sonGratis = qPublicacion.fkTipoPublicacion.tipoPublicacionId.eq(id);
+        return publicacionRepository.findAll(sonGratis, pageRequest).getContent();
+    }
+
+    @Override
+    public long getTotalPublicacionesPagadas() {
+        Long id = TipoPublicacionEnum.PAGADA.getCodigo();
+        Predicate sonPagadas = qPublicacion.fkTipoPublicacion.tipoPublicacionId.eq(id);
+        return publicacionRepository.count(sonPagadas);
+    }
+
+    @Override
+    public long getTotalPublicacionesGratis() {
+        Long id = TipoPublicacionEnum.GRATIS.getCodigo();
+        Predicate sonGratis = qPublicacion.fkTipoPublicacion.tipoPublicacionId.eq(id);
+        return publicacionRepository.count(sonGratis);
+    }
+    
+}
