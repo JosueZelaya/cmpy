bdc77c77020fedb4f3c46ee704164fc7d475abd5
JosueZelaya <josue.a.zelaya@gmail.com>
2016-08-22 10:51:10 -0600

    descomentando linea para insertar el perfil

--- src/main/webapp/WEB-INF/configuracion_inicial.sql
+++ src/main/webapp/WEB-INF/configuracion_inicial.sql
@@ -1,4 +1,4 @@
-#insert into perfil (perfil_id, nombre, sis_activo,creation_time,modification_time,version) values (1,'ROLE_USER',true,current_date,current_date,1);
+insert into perfil (perfil_id, nombre, sis_activo,creation_time,modification_time,version) values (1,'ROLE_USER',true,current_date,current_date,1);
 
 insert into tipo_publicacion (tipo_publicacion_id,nombre,sis_activo,creation_time,modification_time,version) values (1,'PUBLICACION_PAGADA',true,current_date,current_date,1);
 
