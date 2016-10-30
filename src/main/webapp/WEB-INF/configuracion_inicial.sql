0c146f432a922a30386472fe3f7f390d4096f6eb
genaro <genaro@localhost.localdomain>
2016-10-29 20:57:02 -0600

    se agregan insert iniciales de las categorias

--- src/main/webapp/WEB-INF/configuracion_inicial.sql
+++ src/main/webapp/WEB-INF/configuracion_inicial.sql
@@ -3,3 +3,10 @@ insert into perfil (perfil_id, nombre, sis_activo,creation_time,modification_tim
 insert into tipo_publicacion (tipo_publicacion_id,nombre,sis_activo,creation_time,modification_time,version) values (1,'PUBLICACION_PAGADA',true,current_date,current_date,1);
 
 insert into tipo_publicacion (tipo_publicacion_id,nombre,sis_activo,creation_time,modification_time,version) values (2,'PUBLICACION_GRATIS',true,current_date,current_date,1);
+
+insert into categoria (nombre,descripcion,modification_time,creation_time,fk_categoria,sis_activo,version) values ('Multimedia','...',current_date,current_date,null,true,1);
+insert into categoria (nombre,descripcion,modification_time,creation_time,fk_categoria,sis_activo,version) values ('DVD','...',current_date,current_date,1,true,1);
+insert into categoria (nombre,descripcion,modification_time,creation_time,fk_categoria,sis_activo,version) values ('TV','...',current_date,current_date,1,true,1);
+insert into categoria (nombre,descripcion,modification_time,creation_time,fk_categoria,sis_activo,version) values ('SONY','...',current_date,current_date,3,true,1);
+insert into categoria (nombre,descripcion,modification_time,creation_time,fk_categoria,sis_activo,version) values ('RCA','...',current_date,current_date,3,true,1);
+insert into categoria (nombre,descripcion,modification_time,creation_time,fk_categoria,sis_activo,version) values ('LG','...',current_date,current_date,3,true,1);
