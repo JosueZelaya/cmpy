e7a3102af525c566d6aa0012360b5dc357f9cc72
JosueZelaya <josue.a.zelaya@gmail.com>
2017-03-07 23:14:31 -0600

    changed prerender url

--- src/main/java/com/tecnogeek/comprameya/config/AppInitializer.java
+++ src/main/java/com/tecnogeek/comprameya/config/AppInitializer.java
@@ -37,7 +37,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
         com.github.greengerong.PreRenderSEOFilter seoFilter = new com.github.greengerong.PreRenderSEOFilter();
         FilterRegistration.Dynamic filter =  servletContext.addFilter("prerender", seoFilter);
         //filter.setInitParameter("prerenderToken", "bWTX3wnGG43lSldWSr09");
-        filter.setInitParameter("prerenderServiceUrl", "https://comprameya.com:3000/");
+        filter.setInitParameter("prerenderServiceUrl", "http://localhost:3000/");
         filter.addMappingForUrlPatterns(null , true, "/");        
         super.onStartup(servletContext);
     }
