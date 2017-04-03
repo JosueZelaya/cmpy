de3054cf94f323e248706f73f67df0518fe27a42
JosueZelaya <josue.a.zelaya@gmail.com>
2017-04-03 12:17:39 -0600

    pointing to prerender.io service

--- cmpy/src/main/java/com/tecnogeek/comprameya/config/AppInitializer.java
+++ cmpy/src/main/java/com/tecnogeek/comprameya/config/AppInitializer.java
@@ -36,8 +36,8 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     public void onStartup(ServletContext servletContext) throws ServletException {
         com.github.greengerong.PreRenderSEOFilter seoFilter = new com.github.greengerong.PreRenderSEOFilter();
         FilterRegistration.Dynamic filter =  servletContext.addFilter("prerender", seoFilter);
-        //filter.setInitParameter("prerenderToken", "bWTX3wnGG43lSldWSr09");
-        filter.setInitParameter("prerenderServiceUrl", "http://localhost:3000/");
+        filter.setInitParameter("prerenderToken", "bWTX3wnGG43lSldWSr09");
+        filter.setInitParameter("prerenderServiceUrl", "http://service.prerender.io/");
         filter.addMappingForUrlPatterns(null , true, "/");        
         super.onStartup(servletContext);
     }
